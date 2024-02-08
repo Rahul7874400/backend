aa92ff0a39e5565b91911176880af23774a4a81a
Rahul <rkkraj500@gmail.com>
2024-02-08 17:42:36 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/login', (req, res) => {
+    res.send('Login Page')
+  })
+
+  app.get('/twitter',(req,res) =>{
+    res.send('<h1>rkkraj500@gmail.com</h1>')
+  })
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
